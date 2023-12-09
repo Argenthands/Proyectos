f2067a4596cac3e22ec8d7b258900b0134f0145c
Alejandro David Benolol <alejandro.david.benolol@gmail.com>
2023-12-09 10:37:53 -0300

    agregando sonido al video, solo falta configurar el manejo de excepciones y controlar mejor la duración del video para que sin inportar cuantas imagenes nos den dure más o menos lo mismo. tambien faltan las transiciones

--- Python/Projects/MusicAndListOfPicturesToMovie/VideoGenerator.py
+++ Python/Projects/MusicAndListOfPicturesToMovie/VideoGenerator.py
@@ -1,11 +1,18 @@
-from moviepy.editor import ImageSequenceClip, ImageClip, concatenate_videoclips
+from moviepy.editor import ImageSequenceClip, ImageClip, AudioFileClip, concatenate_videoclips
 
-def crear_video(images, output_path='video.mp4', fps=24, remove_temp=True, codec="libx264", audio_codec="aac", video_time=1):
+def crear_video(audio_path, images, output_path='video.mp4', fps=24, remove_temp=True, codec="libx264", audio_codec="aac", video_time=3):
     clips = []
+
+    #Manejo de excepciones:
 
     for image_path in images:
         clip = ImageClip(image_path).set_duration(video_time)  # Puedes ajustar la duración según tus necesidades
         clips.append(clip)
 
     video_clip = concatenate_videoclips(clips, method="compose")
+
+    audio = AudioFileClip(audio_path)
+    audio = audio.set_duration(len(clips) * video_time)
+    video_clip = video_clip.set_audio(audio)
+
     video_clip.write_videofile(output_path, fps=fps, remove_temp=remove_temp, codec=codec, audio_codec=audio_codec)
--- Python/Projects/MusicAndListOfPicturesToMovie/main.py
+++ Python/Projects/MusicAndListOfPicturesToMovie/main.py
@@ -7,4 +7,5 @@ if __name__ == '__main__':
     list_of_images = ['images/Image1.jpg', 'images/Image2.jpg', 'images/Image3.jpg']
     resizor(list_of_images)
     list_of_resized_images = ['images/Image1_resized.jpg', 'images/Image2_resized.jpg', 'images/Image3_resized.jpg']
-    crear_video(list_of_resized_images)
+    music = 'musics/music.mp3'
+    crear_video(audio_path=music, images=list_of_resized_images)
