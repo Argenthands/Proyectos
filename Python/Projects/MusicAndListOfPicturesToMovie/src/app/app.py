2276708cb6cd2b33b55e9c1621040d0383fb24d6
Alejandro David Benolol <alejandro.david.benolol@gmail.com>
2023-12-20 16:32:51 -0300

    ya se cargan los archivos desde la pagina web, falta ver que se rompio en el script a la hora de buscarlos

---
+++ Python/Projects/MusicAndListOfPicturesToMovie/src/app/app.py
@@ -0,0 +1,65 @@
+from flask import Flask, render_template, request, redirect, url_for
+import os
+
+app = Flask(__name__)
+
+# Configuración para subir archivos
+# Configuración para subir archivos
+app.config['UPLOAD_FOLDER_IMAGES'] = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'scripts', 'assets', 'images', 'Input')
+app.config['UPLOAD_FOLDER_SOUND'] = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'scripts', 'assets', 'sound')
+app.config['UPLOAD_FOLDER_VIDEOS'] = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'scripts', 'assets', 'videos', 'Input')
+app.config['ALLOWED_EXTENSIONS'] = {'jpg', 'mp3', 'mp4'}
+
+def create_upload_folders():
+    # Crear carpetas de carga si no existen
+    for folder in [app.config['UPLOAD_FOLDER_IMAGES'], app.config['UPLOAD_FOLDER_SOUND'], app.config['UPLOAD_FOLDER_VIDEOS']]:
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    create_upload_folders()
+    # Manejar el formulario de carga aquí
+    # Verificar si se han subido archivos
+    if 'image[]' in request.files and 'audio' in request.files:
+        # Procesar imágenes
+        images = request.files.getlist('image[]')
+        for image in images:
+            if image and allowed_file(image.filename):
+                image.save(os.path.join(app.config['UPLOAD_FOLDER_IMAGES'], image.filename))
+
+        # Procesar archivo de sonido
+        audio = request.files['audio']
+        if audio and allowed_file(audio.filename):
+            audio.save(os.path.join(app.config['UPLOAD_FOLDER_SOUND'], audio.filename))
+
+        # Procesar archivo de video opcional
+        if 'video' in request.files:
+            video = request.files['video']
+            if video and allowed_file(video.filename):
+                video.save(os.path.join(app.config['UPLOAD_FOLDER_VIDEOS'], video.filename))
+
+        print(f"Ruta completa de guardado: {os.path.abspath(app.config['UPLOAD_FOLDER_IMAGES'])}")
+        # Redirigir a una página de éxito o hacer cualquier otra cosa que necesites
+        return redirect(url_for('success'))
+
+    # Redirigir a una página de error si es necesario
+    return redirect(url_for('error'))
+
+@app.route('/success')
+def success():
+    return 'Archivos subidos con éxito'
+
+@app.route('/error')
+def error():
+    return 'Error al subir archivos'
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- Python/Projects/MusicAndListOfPicturesToMovie/src/app/scripts/main.py
+++ Python/Projects/MusicAndListOfPicturesToMovie/src/app/scripts/main.py
@@ -8,13 +8,18 @@ if __name__ == '__main__':
     resizor(list_of_images)
     list_of_resized_images = Get_elements_from_directory('assets/images/Resized/')
     music = Get_elements_from_directory('assets/sound/')[0]
-    extra_video = Get_elements_from_directory('assets/videos/')
+    extra_video = Get_elements_from_directory('assets/videos/Input/')
     if len(extra_video) > 0:
         extra_video = extra_video[0]
     else:
         extra_video = None
-    create_video(audio_path=music, images=list_of_resized_images, after_or_before='after', extra_video=extra_video)
+    create_video(
+        audio_path=music, 
+        images=list_of_resized_images, 
+        after_or_before='after', 
+        extra_video=extra_video, 
+        output_path='assets/videos/output/video.mp4')
     print(Remove_elements_from_the_directory('assets/images/Resized/'))
     print(Remove_elements_from_the_directory('assets/images/Input/'))
-    print(Remove_elements_from_the_directory('assets/videos/'))
+    print(Remove_elements_from_the_directory('assets/videos/Input/'))
     print(Remove_elements_from_the_directory('assets/sound/'))
